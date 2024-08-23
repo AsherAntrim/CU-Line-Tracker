bb616ee9754e28c625dcbc24e422684049a5559b
Asher <aantrim246@cedarville.edu>
2024-08-23 15:31:11 -0400

    Initial Commit

---
+++ CU Line Tracker/CU_Line_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CU_Line_TrackerApp.swift
+//  CU Line Tracker
+//
+//  Created by Asher Antrim on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CU_Line_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CU Line TrackerTests/CU_Line_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CU_Line_TrackerTests.swift
+//  CU Line TrackerTests
+//
+//  Created by Asher Antrim on 8/23/24.
+//
+
+import Testing
+@testable import CU_Line_Tracker
+
+struct CU_Line_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
